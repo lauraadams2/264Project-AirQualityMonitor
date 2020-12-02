f98e80091fe3da1fa0d4176071e761952fb15340
Laura Adams <laura.adams2233@gmail.com>
2020-12-02 01:10:35 +0000

    working

--- ts_air_quality_logger_ext.py
+++ ts_air_quality_logger_ext.py
@@ -1,62 +1,97 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import time
 from datetime import datetime
-import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import psutil
 from sds011 import *
 import aqi
 
-sensor = SDS011("/dev/ttyAMA0", use_query_mode=True)
+sensor = SDS011("/dev/ttyUSB0", use_query_mode=True)
 
 def get_data(n=3):
 	sensor.sleep(sleep=False)
-	pmt_2_5 = 0
-	pmt_10 = 0
+	pm25 = 0
+	pm10 = 0
 	time.sleep(10)
 	for i in range (n):
 		x = sensor.query()
-		pmt_2_5 = pmt_2_5 + x[0]
-		pmt_10 = pmt_10 + x[1]
+		pm25 = pm25 + x[0]
+		pm10 = pm10 + x[1]
 		time.sleep(2)
-	pmt_2_5 = round(pmt_2_5/n, 1)
-	pm_10 = round(pmt_10/n, 1)
+	pm25 = round(pm25/n, 1)
+	pm10 = round(pm10/n, 1)
 	sensor.sleep(sleep=True)
 	time.sleep(2)
-	return pmt_2_5, pmt_10
-	
+	return pm25, pm10
 
-def conv_aqi(pmt_2_5, pmt_10):
-	aqi_2_5 = aqi.to_iaqi(aqi.POLLUTANT_PM25, str(pmt_2_5))
-	aqi_10 = aqi.to_iaqi(aqi.POLLUTANT_PM10, str(pm_10))
-	return aqi_2_5, aqi_10
-	
-	
+
+def conv_aqi(pm25, pm10):
+	aqi25 = aqi.to_iaqi(aqi.POLLUTANT_PM25, str(pm25))
+	aqi10 = aqi.to_iaqi(aqi.POLLUTANT_PM10, str(pm10))
+	return aqi25, aqi10
+
+
 def save_log():
-	with open("~/264/Airpi/air_quality.csv", "a") as log:
+	with open("/home/rambler/264/AirQualityMonitor/264Project-AirQualityMonitor/air_quality.csv", "a") as log:
 		dt = datetime.now()
-		log.write("{},{},{},{},{}\n".format(dt, pm_2_5, aqi_2_5, pmt_10, aqi_10))
+		log.write("{},{},{},{},{}\n".format(dt, pm25, aqi25, pm10, aqi10))
 	log.close()
-	
-	
+
+
 channelID = "1246085"
-apiKey = "0VGYSVMG9EURZGK6"
-topic = "channels/" + channelID + "/publish/" + apiKey
-mqttHost = "mqtt.thinkspeak.com"
+writeAPIKey = "0VGYSVMG9EURZGK6"
 
-tTransport = "tcp"
-tPort = 1883
-tTLS = None
+mqttUsername = "mwa0000020498094"
+mqttAPIKey = "CF16L07FWJHRDS82"
 
+mqttHost = "mqtt.thingspeak.com"
 
+useUnsecuredTCP = True
+useUnsecuredWebsockets = False
+
+if useUnsecuredWebsockets:
+	tTransport = "websockets"
+	tPort = 80
+	tTLS = None
+
+if useUnsecuredTCP:
+	tTransport = "tcp"
+	tPort = 1883
+	tTLS = None
+
+topic = "channels/" + channelID + "/publish/" + writeAPIKey
+
+"""
+send data to thingspeak.com using 1 of 2 possible connection types: unsecured TCP or unsecured websockets
+"""
 while True:
-	pmt_2_5, pmt_10 = get_data()
-	aqi_2_5, aqi_10 = conv_aqi(pmt_2_5, pmt_10)
-	tPayload = "field1=" + str(pmt_2_5) + "&field2=" + str(aqi_2_5) + "&field3=" + str(pmt_10) + "&field4=" + str(aqi_10)
+	pm25, pm10 = get_data()
+	aqi25, aqi10 = conv_aqi(pm25, pm10)
+
+	payload = "field1=" + str(pm25) + "&field2=" + str(aqi25) + "&field3=" + str(pm10) + "&field4=" + str(aqi10)
 	try:
-		publish.single(topic, payload=tPayload, hostname=mqttHost, port=tPort, tls=tTLS, transport=tTransport)
+		publish.single(topic, payload, hostname=mqttHost, transport=tTransport, port=tPort, tls=tTLS, auth={'username':mqttUsername,'password':mqttAPIKey})
 		save_log()
+		print ("[PM2.5] = ", pm25, "[AQI2.5] = ", aqi25, "[PM10] = ", pm10, "[AQI10] = ", aqi10)
 		time.sleep(60)
+
+	except (KeyboardInterrupt):
+		break
 	except:
 		print ("[INFO] Failure in sending data")
 		time.sleep(12)
+"""
+save to local csv
+"""
+"""
+while(True):
+	pmt_2_5, pmt_10 = get_data()
+	aqi_2_5, aqi_10 = conv_aqi(pmt_2_5, pmt_10)
+	try:
+		save_log()
+	except:
+		print ("[INFO] Failure in logging data")
+	time.sleep(60)
+"""
